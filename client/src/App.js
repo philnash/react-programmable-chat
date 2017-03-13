279caf9df407adcb8a32d29a004b9faad9f419ab
Phil Nash <philnash@gmail.com>
2017-03-13 17:11:39 +0000

    Removes create-react-app boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Hello world!</h2>
       </div>
     );
   }
